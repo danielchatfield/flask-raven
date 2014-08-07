542bb9de23a67ac9b5bfd6c3952888ba4496edec
Daniel Chatfield <chatfielddaniel@gmail.com>
2014-08-07 09:56:50 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='flask_raven',
-    version='0.0.3',
+    version='0.0.4',
     install_requires=["flask", "pycrypto"],
     url='http://github.com/danielchatfield/flask-raven/',
     license='Artistic 2.0',
